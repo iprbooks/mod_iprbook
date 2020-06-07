ecabee059aa7131cdcf3ef11bc47eaac9737093c
andrey <andrey.cherkasov93@gmail.com>
2020-06-07 21:41:33 +0400

    add error handling

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_iprbook';
-$plugin->version = 2020042900;
+$plugin->version = 2020060700;
 $plugin->requires = 2013051401;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.4';
+$plugin->release = 'v1.5';
